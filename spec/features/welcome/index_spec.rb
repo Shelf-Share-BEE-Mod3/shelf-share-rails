37fdecf404f63f549f2ee55026b7d5652d557a54
AJ Tran <ajtrandev@gmail.com>
2020-09-10 14:51:07 -0600

    Add initial spec for landing page
    Co-authored-by: Phillip Strom <62966813+Strompy@users.noreply.github.com>
    Co-authored-by: Jonathan Wilson <53539820+Jonathan-M-Wilson@users.noreply.github.com>
    Co-authored-by: Lito White <62251085+ljwhite@users.noreply.github.com>

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe "Welcome Index Page Spec" do
+  it "I can visit the welcome page" do
+    visit root_path
+    expect(page).to have_content("Welcome to ShelfShare!")
+  end
+end
