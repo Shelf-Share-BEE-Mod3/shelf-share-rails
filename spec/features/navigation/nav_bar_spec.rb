871dd95a80b370cf5b9e99dd724875eef5e494f6
Phillip Strom <phillip.strom@gmail.com>
2020-09-12 13:48:34 -0600

    Add path helpers

--- spec/features/navigation/nav_bar_spec.rb
+++ spec/features/navigation/nav_bar_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe "Navigation Bar" do
 
     within "nav" do
       expect(page).to have_link("Home", href: root_path)
-      expect(page).to have_link("Dashboard")
-      expect(page).to have_link("Find Books")
-      expect(page).to have_link("Friends")
-      expect(page).to have_link("My Books")
-      expect(page).to have_link("Account Info")
+      expect(page).to have_link("Dashboard", href: dashboard_path)
+      expect(page).to have_link("Find Books", href: books_path)
+      expect(page).to have_link("Friends", href: user_friends_path)
+      expect(page).to have_link("My Books", href: user_books_path)
+      expect(page).to have_link("Account Info", href: user_account_path)
     end
-    # Check for links to have the correct xpath once routes exist
+    # Check for links to have the working xpath once routes exist
   end
 end
