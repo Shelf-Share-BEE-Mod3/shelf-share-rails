5cb001f13d7df5668c7087dac3b0af151177ac98
Phillip Strom <phillip.strom@gmail.com>
2020-09-12 15:47:07 -0600

    Add test for multiple friends displaying

--- spec/features/friends/index_spec.rb
+++ spec/features/friends/index_spec.rb
@@ -4,21 +4,35 @@ RSpec.describe 'User Friends Index Page' do
   before :each do
     @user = User.create!(first_name: 'Neal', last_name: 'Stephenson')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    @user2 = User.create!(first_name: 'John', last_name: 'Scalzi')
+    @user3 = User.create!(first_name: 'Robert', last_name: 'Heinlein')
+  end
+  it 'displays no friends when current_user has no friends' do
+    visit user_friends_path
+
+    within ".friend-list" do
+      expect(page).to have_content("no friends")
+    end
   end
 
   it 'has a list of current_users friends' do
-    visit user_dashboard_path
+    @user.friends << @user2
+    @user.friends << @user3
 
-    expect(page).to have_css(".friend-list")
+    visit user_friends_path
 
+    within(".friend-list") do
+      expect(page).to have_link(@user2.first_name)
+      expect(page).to have_link(@user3.first_name)
+    end
   end
 
   it 'has a form to search and add friends' do
-    visit user_dashboard_path
+    visit user_friends_path
 
     expect(page).to have_css(".add-friend-form")
 
-    expect(page).to have_content("Add Friends")
+    expect(page).to have_content("Add friends")
   end
 end
 
