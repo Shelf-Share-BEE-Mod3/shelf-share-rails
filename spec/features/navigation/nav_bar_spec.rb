275d931af93a6d6c1d217997dc3e856fb501f9b8
Phillip Strom <phillip.strom@gmail.com>
2020-09-12 13:52:38 -0600

    Create test for all nav xpaths to hit correct path

--- spec/features/navigation/nav_bar_spec.rb
+++ spec/features/navigation/nav_bar_spec.rb
@@ -12,6 +12,22 @@ RSpec.describe "Navigation Bar" do
       expect(page).to have_link("My Books", href: user_books_path)
       expect(page).to have_link("Account Info", href: user_account_path)
     end
-    # Check for links to have the working xpath once routes exist
+  end
+  it "has working paths for all links" do
+    skip
+  # Check for links to have the working xpath once routes exist
+    visit root_path
+    click_link "Dashboard"
+    expect(current_path).to eq(dashboard_path)
+    click_link "Find Books"
+    expect(current_path).to eq(books_path)
+    click_link "Friends"
+    expect(current_path).to eq(user_friends_path)
+    click_link "My Books"
+    expect(current_path).to eq(user_books_path)
+    click_link "Account Info"
+    expect(current_path).to eq(user_account_path)
+    click_link "Home"
+    expect(current_path).to eq(root_path)
   end
 end
