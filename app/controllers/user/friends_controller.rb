b41e11ab98ebd8890ed82de9146be0cf18887bc9
Phillip Strom <phillip.strom@gmail.com>
2020-09-12 20:42:31 -0600

    Add create action, sad path test passing

--- app/controllers/user/friends_controller.rb
+++ app/controllers/user/friends_controller.rb
@@ -1,3 +1,21 @@
 class User::FriendsController < ApplicationController
   def index; end
+
+  def create
+    friend = User.find_by(email: friends_params[:email])
+    if friend.nil?
+      flash[:error] = 'No users with that email'
+      redirect_to user_friends_path
+    else
+      friend.friend_requests.create(from: current_user)
+      flash[:success] = "Friend Request sent to #{friend.name}"
+      redirect_to user_friends_path
+    end
+  end
+
+  private
+
+  def friends_params
+    params.permit(:email)
+  end
 end
