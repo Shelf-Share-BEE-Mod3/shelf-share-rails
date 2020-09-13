631eb7efd1b765761a31a246c56d9b30ac2a3f6e
Phillip Strom <phillip.strom@gmail.com>
2020-09-13 15:23:20 -0600

    Remove comments

--- app/controllers/user/friends_controller.rb
+++ app/controllers/user/friends_controller.rb
@@ -1,16 +1,3 @@
 class User::FriendsController < ApplicationController
   def index; end
-
-  # def create
-  #   friend = User.find_by(email: friends_params[:email])
-  #   if friend.nil?
-  #     flash[:error] = 'No users with that email'
-  #     redirect_to user_friends_path
-  #   else
-  #     friend.friend_requests.create(from: current_user.id)
-  #     flash[:success] = "Friend Request sent to #{friend.first_name}"
-  #     redirect_to user_friends_path
-  #   end
-  # end
-
 end
