6164472a3f5d65b1baaa1e9585e66c4928c33438
Phillip Strom <phillip.strom@gmail.com>
2020-09-13 15:05:36 -0600

    Move create to friend request controller

--- app/controllers/user/friends_controller.rb
+++ app/controllers/user/friends_controller.rb
@@ -1,21 +1,16 @@
 class User::FriendsController < ApplicationController
   def index; end
 
-  def create
-    friend = User.find_by(email: friends_params[:email])
-    if friend.nil?
-      flash[:error] = 'No users with that email'
-      redirect_to user_friends_path
-    else
-      friend.friend_requests.create(from: current_user.id)
-      flash[:success] = "Friend Request sent to #{friend.first_name}"
-      redirect_to user_friends_path
-    end
-  end
+  # def create
+  #   friend = User.find_by(email: friends_params[:email])
+  #   if friend.nil?
+  #     flash[:error] = 'No users with that email'
+  #     redirect_to user_friends_path
+  #   else
+  #     friend.friend_requests.create(from: current_user.id)
+  #     flash[:success] = "Friend Request sent to #{friend.first_name}"
+  #     redirect_to user_friends_path
+  #   end
+  # end
 
-  private
-
-  def friends_params
-    params.permit(:email)
-  end
 end
