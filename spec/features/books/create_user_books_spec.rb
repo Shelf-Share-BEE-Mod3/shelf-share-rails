4884739ad0a07da0a65ab18211eb63474ee6b301
AJ Tran <ajtrandev@gmail.com>
2020-09-14 14:59:39 -0600

    Update expected paths in create_user_books_spec.rb

--- spec/features/books/create_user_books_spec.rb
+++ spec/features/books/create_user_books_spec.rb
@@ -5,21 +5,17 @@ RSpec.describe 'Create User Books Spec' do
     book = {isbn: '9780441569595', title: 'Neuromancer', author: 'William Gibson'}
 
     login_as_user
-
     visit new_user_book_path
     fill_in :isbn, with: book[:isbn]
     click_button "Add Book to Shelf"
 
-    expect(current_path).to eq(new_user_book_path)
+    expect(current_path).to eq(user_books_path)
     expect(page).to have_content("Added to Shelf: #{book[:title]}, by #{book[:author]}")
-
-    visit user_books_path
     expect(page).to have_content(book[:isbn])
   end
 
   it 'I cannot add a book with an invalid ISBN' do
     login_as_user
-
     visit new_user_book_path
     fill_in :isbn, with: 'x'
     click_button "Add Book to Shelf"
