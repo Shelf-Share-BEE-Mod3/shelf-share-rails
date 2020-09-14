cba8dae6b9a1ac3d9cf673dcaacb5101f19db4b8
AJ Tran <ajtrandev@gmail.com>
2020-09-14 12:56:50 -0600

    Add create_user_books_spec.rb

---
+++ spec/features/books/create_user_books_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Create User Books Spec' do
+  it 'I can add a book by searching its ISBN' do
+    book = {isbn: '9780441569595', title: 'Neuromancer', author: 'William Gibson'}
+
+    login_as_user
+
+    visit new_user_book_path
+    fill_in :isbn, with: book[:isbn]
+    click_button "Add Book to Shelf"
+
+    expect(current_path).to eq(new_user_book_path)
+    expect(page).to have_content("Added to Shelf: #{book[:title]}, by #{book[:author]}")
+
+    visit user_books_path
+    expect(page).to have_content(book[:isbn])
+  end
+end
