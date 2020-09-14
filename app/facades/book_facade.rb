48876c816ddca568c97bcd2c5540f88c675dda44
AJ Tran <ajtrandev@gmail.com>
2020-09-14 15:29:53 -0600

    Update Book Facade to user Book model instead of PORO
    Co-authored-by: Phillip Strom <62966813+Strompy@users.noreply.github.com>
    Co-authored-by: Jonathan Wilson <53539820+Jonathan-M-Wilson@users.noreply.github.com>
    Co-authored-by: Lito White <62251085+ljwhite@users.noreply.github.com>

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,6 +1,14 @@
 class BookFacade
   def self.find_by_isbn(isbn)
-    book_params = BookBuddyService.find_by_isbn(isbn)
-    book = Book.new(book_params)
+    response = BookBuddyService.find_by_isbn(isbn)
+    book_params = response[:data][:attributes]
+    Book.find_or_create_by(isbn: isbn) do |book|
+      book.title = book_params[:title]
+      book.author = book_params[:author]
+      book.description = book_params[:description]
+      book.thumbnail = book_params[:thumbnail]
+      book.isbn = book_params[:isbn]
+      book.category= book_params[:category]
+    end
   end
 end
