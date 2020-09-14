fe828ad39b7603c858df75077a3306ab5d083df6
AJ Tran <ajtrandev@gmail.com>
2020-09-14 15:15:57 -0600

    Comment out book poro

---
+++ app/poros/book_poro.rb
@@ -0,0 +1,14 @@
+# # frozen_string_literal: true
+#
+# class Book
+#   attr_reader :title, :author, :description, :thumbnail, :isbn, :category
+#
+#   def initialize(attributes)
+#     @title = attributes[:data][:attributes][:title]
+#     @author = attributes[:data][:attributes][:author]
+#     @description = attributes[:data][:attributes][:description]
+#     @thumbnail = attributes[:data][:attributes][:thumbnail]
+#     @isbn = attributes[:data][:attributes][:isbn]
+#     @category = attributes[:data][:attributes][:category]
+#   end
+# end
