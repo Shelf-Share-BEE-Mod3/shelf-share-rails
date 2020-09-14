dfadd793ba33df3bcf2d5d0b8b45f24982112358
AJ Tran <ajtrandev@gmail.com>
2020-09-14 14:01:51 -0600

    Add happy path/sad path for creating a UserBook

--- app/controllers/user/books_controller.rb
+++ app/controllers/user/books_controller.rb
@@ -5,6 +5,16 @@ class User::BooksController < ApplicationController
     @userbooks = UserBook.where(user_id: current_user.id)
   end
 
-  def new
+  def new; end
+
+  def create
+    book = BookFacade.find_by_isbn(params[:isbn])
+    user_book = UserBook.new(status: 'available', user_id: current_user.id, isbn: book.isbn)
+    if user_book.save
+      flash[:success] = "Added to Shelf: #{book.title}, by #{book.author}"
+    elsif book.isbn.nil?
+      flash[:not_found] = "Oops! That book cannot be found."
+    end
+    redirect_to new_user_book_path
   end
 end
