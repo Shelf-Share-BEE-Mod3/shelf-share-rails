5beeeddfdd0b6066518801b0cbd2602eb8c70dd1
AJ Tran <ajtrandev@gmail.com>
2020-09-14 14:59:56 -0600

    Update happy/sad paths in create books action

--- app/controllers/user/books_controller.rb
+++ app/controllers/user/books_controller.rb
@@ -8,13 +8,21 @@ class User::BooksController < ApplicationController
   def new; end
 
   def create
+    # if current_user.nil? bounce to 404 page
+    
     book = BookFacade.find_by_isbn(params[:isbn])
+    if book.isbn.nil?
+      flash[:failure] = "Oops! That book cannot be found."
+      redirect_to new_user_book_path and return
+    end
+
     user_book = UserBook.new(status: 'available', user_id: current_user.id, isbn: book.isbn)
     if user_book.save
       flash[:success] = "Added to Shelf: #{book.title}, by #{book.author}"
-    elsif book.isbn.nil?
-      flash[:not_found] = "Oops! That book cannot be found."
+      redirect_to user_books_path
+    else
+      flash[:failure] = "Something happened, try again."
+      redirect_to new_user_book_path
     end
-    redirect_to new_user_book_path
   end
 end
