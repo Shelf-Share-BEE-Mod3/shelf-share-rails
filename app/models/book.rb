bdb3be8bd16fe104ab73a859f01e96659b09ce29
AJ Tran <ajtrandev@gmail.com>
2020-09-15 11:19:14 -0600

    Add book.find_status
    Co-authored-by: Lito White <62251085+ljwhite@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   validates_presence_of :title, :author, :description, :thumbnail, :isbn, :category
   validates_uniqueness_of :isbn
+
+  def find_status
+    UserBook.find_by(isbn: isbn).status
+  end
 end
