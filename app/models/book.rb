c7bf52cecb9d2b1f83bc978836e5aa35537622e4
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-09-15 11:36:26 -0600

    Add user books relationship
    Co-Authored-By: Phillip S <Strompy@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
   validates_presence_of :title, :author, :description, :thumbnail, :isbn, :category
   validates_uniqueness_of :isbn
+
+  has_many :user_books
+  has_many :users, through: :user_books
 end
