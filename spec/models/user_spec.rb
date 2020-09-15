1fa18ed87a82ba43dc6cce2be81399875d1d2878
Phillip Strom <phillip.strom@gmail.com>
2020-09-14 19:30:25 -0600

    Create test for books instance method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,4 +37,30 @@ RSpec.describe User, type: :model do
     expect(new_user.refresh_token).to eq('12345abcdefg')
     expect(new_user.oauth_expires_at).to eq(auth[:credentials][:expires_at])
   end
+  describe 'instance methods' do
+    before :each do
+      auth = {
+        provider: 'google',
+        uid: '12345678910',
+        info: {
+          email: 'lito@litowhite.com',
+          first_name: 'Lito',
+          last_name: 'White'
+        },
+        credentials: {
+          token: 'abcdefg12345',
+          refresh_token: '12345abcdefg',
+          expires_at: DateTime.new(2020, 9, 12, 3, 4, 5)
+        }
+      }
+      User.update_or_create(auth)
+      @new_user = User.first
+    end
+    it "can get books" do
+      book = create(:book)
+      
+      @new_user.user_books.create(isbn: book.isbn)
+      expect(@new_user.books).to eq([book])
+    end
+  end
 end
