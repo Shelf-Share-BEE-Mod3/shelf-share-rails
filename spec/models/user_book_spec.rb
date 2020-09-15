30b0bd790bb926e44871294e587e646e6cb9c3de
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-09-15 11:39:15 -0600

    Remove isbn validation
    Co-Authored-By: Phillip S <Strompy@users.noreply.github.com>

--- spec/models/user_book_spec.rb
+++ spec/models/user_book_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe UserBook, type: :model do
   describe 'validations' do
     it { should validate_presence_of :status }
     it { should validate_presence_of :user_id }
-    it { should validate_presence_of :isbn }
   end
 
   describe 'relationships' do
@@ -20,30 +19,12 @@ RSpec.describe UserBook, type: :model do
       @user = User.create(id: 1)
       @ub1 = UserBook.create({
                                user_id: @user.id,
-                               isbn: @book1.isbn,
                                status: 'available'
                              })
       @ub2 = UserBook.create({
                                user_id: @user.id,
-                               isbn: @book2.isbn,
                                status: 'unavailable'
                              })
-    end
-
-    it 'find_available_userbooks' do
-      expect(UserBook.find_available_userbooks).to eq([@ub1])
-    end
-
-    it 'find_unavailable_userbooks' do
-      expect(UserBook.find_unavailable_userbooks).to eq([@ub2])
-    end
-
-    it "convert_available_userbooks_to_books" do
-      expect(UserBook.convert_available_userbooks_to_books(@user.id)).to eq([@book1])
-    end
-
-    it "convert_unavailable_userbooks_to_books" do
-      expect(UserBook.convert_unavailable_userbooks_to_books(@user.id)).to eq([@book2])
     end
   end
 end
