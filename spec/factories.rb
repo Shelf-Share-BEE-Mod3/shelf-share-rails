6128c436ccdf275f240adb4091b9c94c0080c546
AJ Tran <ajtrandev@gmail.com>
2020-09-15 08:56:17 -0600

    Add User factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,4 +7,16 @@ FactoryBot.define do
     thumbnail { 'https://upload.wikimedia.org/wikipedia/commons/b/b8/Indian_Election_Symbol_Book.svg' }
     category { Faker::Book.genre }
   end
+
+  factory :user do
+    faker_omniauth = Faker::Omniauth.new
+    provider { 'google' }
+    uid { Faker::Internet.uuid }
+    email { faker_omniauth.email }
+    first_name { faker_omniauth.first_name }
+    last_name { faker_omniauth.last_name }
+    token { 'TOKEN' }
+    refresh_token { 'REFRESH_TOKEN' }
+    oauth_expires_at { }
+  end
 end
