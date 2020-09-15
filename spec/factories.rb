a751dc4addd0420d52223c61ef2389120282cae3
AJ Tran <ajtrandev@gmail.com>
2020-09-15 08:38:23 -0600

    Update book factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     title { Faker::Book.title }
     author { Faker::Book.author }
     description { Faker::Lorem.sentence }
-    isbn { Faker::Code.isbn }
+    isbn { Faker::Code.isbn(base: 13).tr('-', '') }
     thumbnail { 'https://upload.wikimedia.org/wikipedia/commons/b/b8/Indian_Election_Symbol_Book.svg' }
     category { Faker::Book.genre }
   end
