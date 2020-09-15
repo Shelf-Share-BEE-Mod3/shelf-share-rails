6d0a5536d3fcdc62aa1aa0ce49be90b13fe16ad3
Phillip Strom <phillip.strom@gmail.com>
2020-09-14 18:56:17 -0600

    Update test to match new Book model

--- spec/features/books/find_books_index_spec.rb
+++ spec/features/books/find_books_index_spec.rb
@@ -6,66 +6,54 @@ RSpec.describe "Find Books Index" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     @user2 = User.create!(first_name: 'John', last_name: 'Scalzi')
     @user3 = User.create!(first_name: 'Robert', last_name: 'Heinlein')
-    # make books
+
     5.times do
       @user2.user_books.create(
         status: 'available',
-        isbn: rand(9999999999999).to_s.center(13, rand(9).to_s)
+        isbn: Faker::Code.isbn(base: 13).tr('-', '')
       )
     end
+
     5.times do
       @user3.user_books.create(
         status: 'available',
-        isbn: rand(9999999999999).to_s.center(13, rand(9).to_s)
+        isbn: Faker::Code.isbn(base: 13).tr('-', '')
       )
     end
+
     attributes = {
-    items: [
-      {
-        volumeInfo: {
-          title: "Ender's Game",
-          authors: [
-            "Orson Scott Card"
-            ],
-            description: "An expert at simulated war games, Andrew \"Ender\" Wiggin believes that he is engaged in one more computer war game when, in truth, he is commanding the last Earth fleet against an alien race seeking Earth's complete destruction.",
-            industryIdentifiers: [
-              {
-                type: "ISBN_13",
-                identifier: "9780765370624"
-              }
-            ],
-          categories: [
-            "Fiction"
-            ],
-          imageLinks: {
-            thumbnail: "http://books.google.com/books/content?id=WEW1cC7yaCQC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-            }
+      data: {
+        id: '1',
+        type: 'book',
+        attributes: { title: "Ender's Game",
+          author: 'Orson Scott Card',
+          description: "An expert at simulated war games, Andrew \"Ender\" Wiggin believes that he is engaged in one more computer war game when, in truth, he is commanding the last Earth fleet against an alien race seeking Earth's complete destruction.",
+          thumbnail: 'http://books.google.com/books/content?id=WEW1cC7yaCQC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+          isbn: '9780765370624',
+          category: 'Fiction'
           }
         }
-      ]
-    }
-    @book_poro = Book.new(attributes)
-    allow(Book).to receive(:new).and_return(@book_poro)
+      }
+
+    allow(BookBuddyService).to receive(:find_by_isbn).and_return(attributes)
 
     @user.friends << @user2
     @user.friends << @user3
-    @book1 = UserBook.first
-    # @book2 = UserBook.last
   end
   it "shows list of books organized by friend" do
     visit books_path
+
+    book = Book.first
 
     expect(page).to have_content("Browse Books")
     expect(page).to have_content("Sorted by Friend")
     expect(page).to have_css('.friend-book-shelf', count: 2)
     expect(page).to have_css('.book', count: 10)
     within(first(".book")) do
-      # expect(page).to have_content(@book1.isbn)
-
-      expect(page).to have_content(@book_poro.title)
-      expect(page).to have_content(@book_poro.author)
-      expect(page).to have_content(@book_poro.category)
-      expect(page).to have_css("img[src*='#{@book_poro.thumbnail}']")
+      expect(page).to have_content(book.title)
+      expect(page).to have_content(book.author)
+      expect(page).to have_content(book.category)
+      expect(page).to have_css("img[src*='#{book.thumbnail}']")
     end
   end
 end
