fa0bd4246198eea3a9a596fdf58ab2d610cde255
AJ Tran <ajtrandev@gmail.com>
2020-09-15 09:41:18 -0600

    Add link to Friend Book Show page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,8 @@ class BooksController < ApplicationController
   def index
     @friends = current_user.friends.includes(:user_books)
   end
+
+  def show
+    @book = Book.find(params[:id])
+  end
 end
