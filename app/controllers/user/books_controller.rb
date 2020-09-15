77fb7ff35f5d08e05865934326a64589b79d1e39
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-09-15 11:41:35 -0600

    Update controller to use new methods
    Co-Authored-By: Phillip S <Strompy@users.noreply.github.com>

--- app/controllers/user/books_controller.rb
+++ app/controllers/user/books_controller.rb
@@ -2,22 +2,21 @@
 
 class User::BooksController < ApplicationController
   def index
-    @available_books = UserBook.convert_available_userbooks_to_books(current_user.id)
-    @unavailable_books = UserBook.convert_unavailable_userbooks_to_books(current_user.id)
+    @available_books = current_user.available_books
+    @unavailable_books = current_user.unavailable_books
   end
 
   def new; end
 
   def create
     # if current_user.nil? bounce to 404 page
-
     book = BookFacade.find_by_isbn(params[:isbn])
     if book.isbn.nil?
       flash[:failure] = "Oops! That book cannot be found."
       redirect_to new_user_book_path and return
     end
 
-    user_book = UserBook.new(status: 'available', user_id: current_user.id, isbn: book.isbn)
+    user_book = UserBook.new(status: 'available', user_id: current_user.id, book_id: book.id)
     if user_book.save
       flash[:success] = "Added to Shelf: #{book.title}, by #{book.author}"
       redirect_to user_books_path
