9c2a36783372dc13ac606ff6b757014a109362fe
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-09-16 16:47:50 -0600

    Remind user to write down address
    Co-Authored-By: Lito White <ljwhite@users.noreply.github.com>

--- app/controllers/return_controller.rb
+++ app/controllers/return_controller.rb
@@ -6,6 +6,7 @@ class ReturnController < ApplicationController
 
   def show
     @friend = User.find(params[:id])
+    @user_book = UserBook.find(params[:user_book_id])
     StatusFacade.change_user_book_status_to_available(params[:user_book_id])
     StatusFacade.change_borrow_request_status_to_returned(params[:borrow_request_id])
   end
