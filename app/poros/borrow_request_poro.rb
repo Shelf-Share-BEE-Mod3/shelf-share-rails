2f5e5a037023e115c24ff58927240d7bcd144890
AJ Tran <ajtrandev@gmail.com>
2020-09-16 16:02:16 -0600

    Add BorrowRequestPoro
    Co-authored-by: Phillip Strom <62966813+Strompy@users.noreply.github.com>

--- app/poros/borrow_request_poro.rb
+++ app/poros/borrow_request_poro.rb
@@ -1,7 +1,8 @@
 class BorrowRequestPoro
-
-  def initialize
-    
+  attr_reader :book_title, :belongs_to, :borrower
+  def initialize(params)
+    @book_title = params[:book_title]
+    @belongs_to = params[:belongs_to]
+    @borrower = params[:borrower]
   end
-
 end
