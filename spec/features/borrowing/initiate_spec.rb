30717d1124b00a04ee6a46a0a9fcd3ac4b4239b5
AJ Tran <ajtrandev@gmail.com>
2020-09-16 07:22:16 -0600

    Update spec

--- spec/features/borrowing/initiate_spec.rb
+++ spec/features/borrowing/initiate_spec.rb
@@ -5,15 +5,19 @@ RSpec.describe "Borrowing Spec 1/?" do
     @user1, @user2 = create_list(:user, 2)
     @user1.friends << @user2
     @book = create(:book)
-    create(:user_book, user: @user2, book: @book)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+    @user_book = create(:user_book, user: @user2, book: @book)
   end
 
   it "I can send a borrow request from my friend's book show page" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+
     expect do
       visit books_path
       click_link @book.title
       click_button "Ask to Borrow"
     end.to change { BorrowRequest.count }.by(1)
+
+    expect(current_path).to eq(books_path)
+    expect(page).to have_content("Borrow Request sent to #{@user2.first_name}")
   end
 end
