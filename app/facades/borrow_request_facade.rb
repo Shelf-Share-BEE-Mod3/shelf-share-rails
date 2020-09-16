92ee71abdbb3747959d25b4ca3f5d4f0beb978dd
AJ Tran <ajtrandev@gmail.com>
2020-09-16 16:06:35 -0600

    Add Borrow Request Facade
    Co-authored-by: Phillip Strom <62966813+Strompy@users.noreply.github.com>

--- app/facades/borrow_request_facade.rb
+++ app/facades/borrow_request_facade.rb
@@ -1,8 +1,12 @@
 class BorrowRequestFacade
   def self.incoming_book_borrow_requests(user)
     user.incoming_book_borrow_requests.map do |request|
-        # BorrowRequestPoro.new(request)
-        require "pry"; binding.pry
+      params = {
+        belongs_to: user.full_name,
+        borrower: request.borrower.full_name,
+        book_title: request.user_book.book.title
+      }
+      BorrowRequestPoro.new(params)
     end
   end
 
