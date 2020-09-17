dd85ab7475d0dcfc2977759628282760e1ecadb8
AJ Tran <ajtrandev@gmail.com>
2020-09-17 02:41:39 -0600

    Update borrowing/initiate spec

--- spec/features/borrowing/initiate_spec.rb
+++ spec/features/borrowing/initiate_spec.rb
@@ -5,15 +5,21 @@ RSpec.describe "Borrowing Spec 1/?" do
     @user1, @user2 = create_list(:user, 2)
     @address = create(:address, user: @user1, address_second: "Apt 101")
     @user1.friends << @user2
+    @user2.friends << @user1
     @book = create(:book)
     @user_book = create(:user_book, user: @user2, book: @book)
   end
 
   it "I can send a borrow request from my friend's book show page" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user2)
+    visit user_dashboard_path
+    within ".book-requests" do
+      expect(page).to have_content("No current requests")
+    end
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
 
     expect(BorrowRequest.count).to eq(0)
-
     expect do
       visit books_path
       click_link @book.title
