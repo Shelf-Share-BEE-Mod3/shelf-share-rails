d78039647b282175257ad219879445f8676bf328
Phillip Strom <phillip.strom@gmail.com>
2020-09-16 20:22:39 -0600

    Add destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     get '/dashboard', to: 'dashboard#show', as: 'dashboard'
     resources :friends, only: [:index, :show]
     resources :friend_requests, only: %i[create update destroy]
-    resources :books, only: [:index, :new, :create, :show]
+    resources :books, only: [:index, :new, :create, :show, :destroy]
     get '/account', to: 'account#show'
   end
 
