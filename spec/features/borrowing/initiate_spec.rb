187bd40475bd5a556720a27ea637897c5f7426da
AJ Tran <ajtrandev@gmail.com>
2020-09-17 09:48:26 -0600

    Create new initiate spec
      it's awesome, seriously awesome
    
    Co-authored-by: Phillip Strom <62966813+Strompy@users.noreply.github.com>
    Co-authored-by: Jonathan Wilson <53539820+Jonathan-M-Wilson@users.noreply.github.com>
    Co-authored-by: Lito White <62251085+ljwhite@users.noreply.github.com>

--- spec/features/borrowing/initiate_spec.rb
+++ spec/features/borrowing/initiate_spec.rb
@@ -1,18 +1,27 @@
 require "rails_helper"
 
 RSpec.describe "Borrowing Spec 1/?" do
-  it "I can send a book borrow request to my friend"
+  before :each do
+    @borrower = create(:borrower)
+    @librarian = create(:user)
+    @address1 = create(:address, user: @borrower, address_second: "Apt 101")
+    @address2 = create(:address, user: @librarian, address_second: "Apt 222")
+    @borrower.friends << @librarian
+    @librarian.friends << @borrower
+    @book = create(:book)
+    @user_book = create(:user_book, user: @librarian, book: @book)
+    # @borrow_request = create(:borrow_request, borrower: @borrower, user_book: @user_book)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@borrower)
+    # user 1 is borrowing from user 2
+  end
+  it "I can send a book borrow request to my friend" do
+    # logged in as @borrower
+    visit books_path
+    click_link @book.title
+    expect(current_path).to eq(book_path(@book))
 
-  # TODO
-  # if i don't have an address when i ask to borrow, i will be prompted to do so
-
-  # creating a borrow request will:
-  # 1. record a borrow request with a status pending
-  # 2. remove the option for me to ask again for that book
-
-  # the pattern is back and forth between users, like friend requests
-
-  # it should not affect the status of any user_book records
-
-  # it needs to be tested in the browser
+    expect do
+      click_button "Ask to Borrow"
+    end.to change { BorrowRequest.count }.by(1)
+  end
 end
