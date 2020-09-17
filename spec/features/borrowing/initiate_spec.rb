a5c2c6ec6450e303c4637fc9853278cacfb67495
AJ Tran <ajtrandev@gmail.com>
2020-09-17 04:49:10 -0600

    Add comment notes about borrowing into spec

--- spec/features/borrowing/initiate_spec.rb
+++ spec/features/borrowing/initiate_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "Borrowing Spec 1/?" do
-  it "I can send a book borrow request to my friend" 
+  it "I can send a book borrow request to my friend"
+
+  # if i don't have an address when i ask to borrow, i will be prompted to do so
+  # creating a borrow request will:
+  # 1. record a borrow request with a status pending
+  # 2. remove the option for me to ask again for that book
 end
