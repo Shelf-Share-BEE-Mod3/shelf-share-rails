b2644647cb401a575ffd6e5440c4b28e8bc33ad3
AJ Tran <ajtrandev@gmail.com>
2020-09-16 23:23:09 -0600

    Add sad path to address prompt workflow

--- spec/features/address/prompt_for_address_after_login_spec.rb
+++ spec/features/address/prompt_for_address_after_login_spec.rb
@@ -70,4 +70,33 @@ RSpec.describe "Address Prompt Page Spec" do
     expect(page).to have_content(@message)
     expect(page).to have_link('profile page')
   end
+
+
+  scenario 'Submitting an incomplete form redirects me to the form page with a flash message indicating the error. All fields are pre-populated with what was initially submitted' do
+    address = build(:address, address_second: "Apt 101")
+
+    login_as_user
+    expect(current_path).to eq('/address/prompt')
+
+    within ".new-address-form" do
+      fill_in :address_first, with: address.address_first
+      fill_in :address_second, with: address.address_second
+      # fill_in :city, with: address.city
+      fill_in :state, with: address.state
+      fill_in :zip, with: address.zip
+      click_button 'Submit Address'
+    end
+
+    expect(current_user.address).to be_falsey
+    expect(current_path).to eq('/address/prompt')
+    expect(page).to have_content('Please fill out all required fields')
+    expect(page).to have_content("City can't be blank")
+    expect(page).to_not have_field(:city, with: address.city)
+
+    expect(page).to have_field(:address_first, with: address.address_first)
+    expect(page).to have_field(:address_second, with: address.address_second)
+    expect(page).to have_field(:state, with: address.state)
+    expect(page).to have_field(:zip, with: address.zip)
+
+  end
 end
