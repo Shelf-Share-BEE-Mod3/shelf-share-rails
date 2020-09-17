f50079e2c0b7ddc8ed18570cf07f39b4930db8f0
Phillip Strom <phillip.strom@gmail.com>
2020-09-17 16:26:23 -0600

    Add includes statements for optimization

--- app/controllers/borrow_requests_controller.rb
+++ app/controllers/borrow_requests_controller.rb
@@ -1,7 +1,7 @@
 class BorrowRequestsController < ApplicationController
   def index
     @incoming_requests = current_user.incoming_book_borrow_requests
-    @outgoing_requests = current_user.borrow_requests.where(status: 'pending')
+    @outgoing_requests = current_user.borrow_requests.where(status: 'pending').includes(user_book: :book)
   end
 
   def show
