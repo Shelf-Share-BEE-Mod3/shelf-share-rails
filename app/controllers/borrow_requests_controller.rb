c2b90fe3448e64774c1381db49e19aa54272b904
AJ Tran <ajtrandev@gmail.com>
2020-09-17 04:07:56 -0600

    Add feature: a user can approve borrow requests
    The controller has been refactored to take an update action on approvals

--- app/controllers/borrow_requests_controller.rb
+++ app/controllers/borrow_requests_controller.rb
@@ -9,24 +9,29 @@ class BorrowRequestsController < ApplicationController
     @address = Address.includes(:user).find_by(user_id: borrow_request.borrower_id)
   end
 
-  def create
-    friend_book = UserBook.where(user_id: params[:friend_id], book_id: params[:friend_book_id]).first
-    if friend_book.status == 'available'
-      BorrowRequest.create(status: 0, user_book: friend_book, borrower_id: params[:user_id])
-      friend_book.update(status: 'unavailable')
-      flash[:success] = "Borrow Request sent to #{User.find(params[:friend_id]).full_name}"
-      redirect_to books_path
-    end
-  end
-
   def update
     borrow_request = BorrowRequest.find(params[:id])
-    borrow_request.declined!
-    decline_message = "You have declined #{borrow_request.borrower.first_name}'s " +
-                      "request to borrow #{borrow_request.user_book.book.title}"
-    flash[:success] = decline_message
-    redirect_to user_dashboard_path
+    borrow_request.approve_request
+    redirect_to address_path(borrow_request.borrower.address)
   end
+  # def create
+  #   friend_book = UserBook.where(user_id: params[:friend_id], book_id: params[:friend_book_id]).first
+  #   if friend_book.status == 'available'
+  #     BorrowRequest.create(status: 0, user_book: friend_book, borrower_id: params[:user_id])
+  #     friend_book.update(status: 'unavailable')
+  #     flash[:success] = "Borrow Request sent to #{User.find(params[:friend_id]).full_name}"
+  #     redirect_to books_path
+  #   end
+  # end
+  #
+  # def update
+  #   borrow_request = BorrowRequest.find(params[:id])
+  #   borrow_request.declined!
+  #   decline_message = "You have declined #{borrow_request.borrower.first_name}'s " +
+  #                     "request to borrow #{borrow_request.user_book.book.title}"
+  #   flash[:success] = decline_message
+  #   redirect_to user_dashboard_path
+  # end
 
   private
 
