43b6326d8f12d0492f3a0dd2bc3cf0a77bea5d24
Phillip Strom <phillip.strom@gmail.com>
2020-09-17 20:02:14 -0600

    Add borrowed_books instance variable for the view

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -2,5 +2,6 @@ class User::DashboardController < ApplicationController
   def show
     @friend_requests = current_user.current_friend_requests
     @book_requests = BorrowRequestFacade.incoming_book_borrow_requests(current_user)
+    @borrowed_books = current_user.borrow_requests.find_approved_requests
   end
 end
