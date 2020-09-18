b2b197f796d6e3a675aafd5d31b725421b0f1e02
Lito White <leland.white@gmail.com>
2020-09-18 00:32:36 -0600

    test suite passing

--- spec/features/books/user_books_index_spec.rb
+++ spec/features/books/user_books_index_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe 'User Book Index Page' do
     login_as_user
     @book1 = create(:book)
     @book2 = create(:book)
+    # @book1.user_booka.create(user_id: current_user.id, status: 'available')
+    # @book2.userbook.create(user_id: current_user.id, status: 'available')
   end
 
   it 'My books show up on the shelf' do
@@ -21,10 +23,12 @@ RSpec.describe 'User Book Index Page' do
                       status: 'unavailable'
                     })
     visit user_books_path
-    expect(page).to have_content(@book1.title)
-    expect(page).to have_content(@book2.title)
+    expect(page).to have_css("img[src*='#{@book1.thumbnail}']")
+    expect(page).to have_css("img[src*='#{@book2.thumbnail}']")
     expect(page).to have_content("On The Shelf")
     expect(page).to have_content("Off The Shelf")
+    expect(page).to have_css(".available_book", count: 1)
+    expect(page).to have_css(".unavailable_book", count: 1)
   end
 
   it 'If I have no available books, the page will indicate it' do
@@ -39,11 +43,10 @@ RSpec.describe 'User Book Index Page' do
                       status: 'unavailable'
                     })
     visit user_books_path
-    expect(page).to have_content(@book1.title)
-    expect(page).to have_content(@book2.title)
-    expect(page).to have_content("On The Shelf")
-    expect(page).to have_content("Off The Shelf")
+    expect(page).to have_css("img[src*='#{@book1.thumbnail}']")
+    expect(page).to have_css("img[src*='#{@book2.thumbnail}']")
     expect(page).to have_content("No books are currently available")
+    expect(page).to have_css(".unavailable_book", count: 2)
   end
 
   it 'If I have no unavailable books, the page will indicate it' do
@@ -58,11 +61,26 @@ RSpec.describe 'User Book Index Page' do
                       status: 'available'
                     })
     visit user_books_path
-    expect(page).to have_content(@book1.title)
-    expect(page).to have_content(@book2.title)
-    expect(page).to have_content("On The Shelf")
-    expect(page).to have_content("Off The Shelf")
+    expect(page).to have_css("img[src*='#{@book1.thumbnail}']")
+    expect(page).to have_css("img[src*='#{@book2.thumbnail}']")
     expect(page).to have_content("No books are currently unavailable")
-    save_and_open_page
+  end
+
+  it "The book cover is a link that takes me to the user book show page" do
+    UserBook.create({
+                      user_id: current_user.id,
+                      book_id: @book1.id,
+                      status: 'available'
+                    })
+    UserBook.create({
+                      user_id: current_user.id,
+                      book_id: @book2.id,
+                      status: 'unavailable'
+                    })
+    visit user_books_path
+    within('.available_book') do
+      find(:xpath, "//a[contains(@href,#{@book1.id})]").click
+    end
+    expect(current_path).to eq(user_book_path(@book1))
   end
 end
